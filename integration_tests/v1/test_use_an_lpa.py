581afdbaf3174296f4f3d50998856f47a840bb0a
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-07-28 16:37:29 +0100

    Sirius service test, marked the integrations tests up properly

--- integration_tests/v1/test_use_an_lpa.py
+++ integration_tests/v1/test_use_an_lpa.py
@@ -2,7 +2,7 @@ import pytest
 import json
 from .conftest import send_a_request, configs_to_test, is_valid_schema
 
-
+@pytest.mark.smoke_test
 @pytest.mark.parametrize("test_config", configs_to_test)
 def test_online_tool_route(test_config):
 
@@ -18,7 +18,7 @@ def test_online_tool_route(test_config):
         assert is_valid_schema(response_dict, "use_an_lpa_schema.json")
         assert response_dict["uId"] == valid_id
 
-
+@pytest.mark.smoke_test
 @pytest.mark.parametrize("test_config", configs_to_test)
 def test_online_tool_route_invalid_id(test_config):
 
--- lambda_functions/v1/functions/lpa/app/api/sirius_service.py
+++ lambda_functions/v1/functions/lpa/app/api/sirius_service.py
@@ -6,6 +6,7 @@ from .helpers import custom_logger
 logger = custom_logger("sirius_service")
 
 
+
 def build_sirius_url(base_url, version, endpoint, url_params=None):
     """
     Builds the url for the endpoint from variables (probably saved in env vars)
@@ -18,7 +19,9 @@ def build_sirius_url(base_url, version, endpoint, url_params=None):
         string: url
     """
 
-    sirius_url = f"{base_url}/{version}/{endpoint}"
+    url_parts = [base_url, version, endpoint]
+
+    sirius_url = "/".join([i for i in url_parts if i is not None])
 
     if url_params:
         encoded_params = urlencode(url_params)
@@ -28,6 +31,6 @@ def build_sirius_url(base_url, version, endpoint, url_params=None):
 
     if urlparse(url).scheme not in ["https", "http"]:
         logger.info("Unable to build Sirius URL")
-        return False
+        raise Exception
 
     return url
