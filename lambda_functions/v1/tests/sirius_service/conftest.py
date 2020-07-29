2ef092d50847b9c671ccb16217e386b0a3a9777b
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-07-29 16:28:52 +0100

    Experimenting with hypothesis for auto generating test cases

--- lambda_functions/v1/tests/sirius_service/conftest.py
+++ lambda_functions/v1/tests/sirius_service/conftest.py
@@ -8,6 +8,11 @@ import os
 import requests
 
 from lambda_functions.v1.functions.lpa.app.api import sirius_service
+import os
+
+
+# Defaults to 50
+max_examples = int(os.environ["HYPOTHESIS_MAX_EXAMPLES"])
 
 max_examples = int(os.environ["HYPOTHESIS_MAX_EXAMPLES"])
 
