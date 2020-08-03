49a00a43c8cdb9b70d39f12e6cc567cffcb077bb
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-08-03 15:36:53 +0100

    Integration tests all pass

--- integration_tests/v1/test_use_an_lpa.py
+++ integration_tests/v1/test_use_an_lpa.py
@@ -5,7 +5,7 @@ from .conftest import send_a_request, configs_to_test, is_valid_schema
 
 @pytest.mark.smoke_test
 @pytest.mark.parametrize("test_config", configs_to_test)
-def test_online_tool_route(test_config):
+def test_use_an_lpa_route(test_config):
 
     for valid_id in test_config["use_an_lpa_endpoint"]["valid_sirius_uids"]:
         status, response = send_a_request(
@@ -16,14 +16,14 @@ def test_online_tool_route(test_config):
 
         assert status == 200
         response_dict = json.loads(response)
-        assert is_valid_schema(response_dict, "use_an_lpa_schema.json")
-        assert response_dict["uId"].replace("-", "") == valid_id
+        # TODO we are getting a list from sirius?
+        assert is_valid_schema(response_dict[0], "use_an_lpa_schema.json")
+        assert response_dict[0]["uId"].replace("-", "") == valid_id
 
 
-@pytest.mark.xfail(reason="not catching if sirius response is empty, returns 500")
 @pytest.mark.smoke_test
 @pytest.mark.parametrize("test_config", configs_to_test)
-def test_online_tool_route_invalid_id(test_config):
+def test_use_an_lpa_route_invalid_id(test_config):
 
     for valid_id in test_config["use_an_lpa_endpoint"]["invalid_sirius_uids"]:
         status, response = send_a_request(
@@ -33,5 +33,3 @@ def test_online_tool_route_invalid_id(test_config):
         )
 
         assert status == 404
-        response_dict = json.loads(response)
-        assert response_dict == {}
