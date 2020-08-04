9622f11572de2f7eb9a3811f0df2dbd77b6f586e
james.warren <james.warren@digital.justice.gov.uk>
2020-08-04 11:58:39 +0100

    IN-322 fix all the terraform workarounds

--- terraform/environment/api_rest.tf
+++ terraform/environment/api_rest.tf
@@ -3,17 +3,18 @@ data "template_file" "_" {
   vars     = local.api_template_vars
 }
 
-// Bug - Recreates api gateway spec on each build!
-// Can't use Lifecycle ignore changes as not attaching policy on first build!
-// https://github.com/terraform-providers/terraform-provider-aws/issues/5549
 resource "aws_api_gateway_rest_api" "lpa" {
-  name        = "lpa-${local.environment}"
-  description = "API Gateway for LPA Data - ${local.environment}"
-  policy      = data.aws_iam_policy_document.resource_policy.json
-  body        = data.template_file._.rendered
+  name = "lpa-${local.environment}"
+  body = data.template_file._.rendered
 
   endpoint_configuration {
     types = ["REGIONAL"]
+  }
+  lifecycle {
+    ignore_changes = [
+      policy,
+      description
+    ]
   }
   tags = local.default_tags
 }
--- terraform/environment/modules/stage/deploy.tf
+++ terraform/environment/modules/stage/deploy.tf
@@ -1,14 +1,11 @@
 locals {
-  openapi_sha               = var.openapi_version == "v1" ? "fixed_variable" : substr(replace(base64sha256(data.local_file.openapispec.content_base64), "/[^0-9A-Za-z_]/", ""), 0, 5)
-  lambda_version_folder_sha = var.openapi_version == "v1" ? "fixed_variable" : substr(replace(base64sha256(data.local_file.lambda_version_folder_sha.content_base64), "/[^0-9A-Za-z_]/", ""), 0, 5)
+  openapi_sha = substr(replace(base64sha256(data.local_file.openapispec.content_base64), "/[^0-9A-Za-z_]/", ""), 0, 5)
+  //Modify here for new version - uncomment below and modify accordingly
+  //  openapi_sha               = var.openapi_version == "v1" ? "fixed_variable" : substr(replace(base64sha256(data.local_file.openapispec.content_base64), "/[^0-9A-Za-z_]/", ""), 0, 5)
 }
 
 data "local_file" "openapispec" {
   filename = "../../lambda_functions/${var.openapi_version}/openapi/${var.api_name}-openapi.yml"
-}
-
-data "local_file" "lambda_version_folder_sha" {
-  filename = "../../lambda_functions/${var.openapi_version}/directory_sha"
 }
 
 resource "aws_api_gateway_deployment" "deploy" {
@@ -18,7 +15,7 @@ resource "aws_api_gateway_deployment" "deploy" {
     // Force a deploy on when content has changed
     stage_version             = var.openapi_version
     content_api_sha           = local.openapi_sha
-    lambda_version_folder_sha = local.lambda_version_folder_sha
+    lambda_version_folder_sha = var.lpa_lambda.source_code_hash
   }
   lifecycle {
     create_before_destroy = true
