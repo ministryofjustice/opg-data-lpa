e815690c7548f4492f264ced4b8cd97a1cdbce02
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-08-04 10:19:01 +0100

    Mock sirius for LPA endpoints

---
+++ mock_sirius_backend/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import json
+import connexion
+from flask import Response, jsonify, request
+import requests
+import logging
+
+from api.lpas.handlers import handle_lpa_get
+
+
+def getLpas(*args, **kwargs):
+
+    status_code, response_message = handle_lpa_get(query_params=kwargs)
+    logging.info(f"status_code: {status_code}")
+    logging.info(f"response_message: {response_message}")
+
+    return response_message, int(status_code)
+
+
+
+logging.basicConfig(level=logging.INFO)
+sirius_server = connexion.FlaskApp(__name__)
+sirius_server.add_api("sirius_public_api.yaml")
+sirius_server.run(port=5001)
