dc2d6c73c337d790f29927809926189208cfd4c1
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-11-13 10:44:18 +0000

    cd to directory of the script in the event that the bash file is called from elsewhere

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+cd $(dirname $BASH_SOURCE)
 $(aws-vault exec identity -- go run ./docs/support_scripts/aws/getcreds.go)
-docker-compose -f docker-compose.yml build \
+docker-compose -f docker-compose.yml build --no-cache \
 --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
 --build-arg AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
