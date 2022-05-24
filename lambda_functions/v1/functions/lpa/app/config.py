7d9a79900d2f9475ac7872288893e2abd105a57f
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-05-24 20:09:33 +0100

    ttl is a number

--- lambda_functions/v1/functions/lpa/app/config.py
+++ lambda_functions/v1/functions/lpa/app/config.py
@@ -18,7 +18,7 @@ class Config(object):
 
     REDIS_URL = os.environ.get("REDIS_URL")
     REQUEST_CACHING = os.environ.get("REQUEST_CACHING", default="disabled")
-    REQUEST_CACHING_TTL = os.environ.get("REQUEST_CACHING_TTL", default="48")
+    REQUEST_CACHING_TTL = int(os.environ.get("REQUEST_CACHING_TTL", default="48"))
     REQUEST_TIMEOUT = float(os.environ.get("REQUEST_TIMEOUT", default="10"))
 
 
