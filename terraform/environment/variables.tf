774185a3aa59dee304c7feb15a6f70f10b529f92
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2022-05-24 17:47:51 +0100

    add variable to configure request caching ttl

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -9,16 +9,17 @@ variable "management_role" {
 variable "accounts" {
   type = map(
     object({
-      account_id         = string
-      account_mapping    = string
-      allowed_roles      = list(string)
-      is_production      = string
-      logger_level       = string
-      opg_hosted_zone    = string
-      vpc_id             = string
-      session_data       = string
-      target_environment = string
-      elasticache_count  = number
+      account_id          = string
+      account_mapping     = string
+      allowed_roles       = list(string)
+      is_production       = string
+      logger_level        = string
+      opg_hosted_zone     = string
+      vpc_id              = string
+      session_data        = string
+      target_environment  = string
+      elasticache_count   = number
+      request_caching_ttl = number
     })
   )
 }
