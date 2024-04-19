b45eebffe580224a630ce4f278bd5f2e3dba7fdb
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2024-04-19 11:28:25 +0100

    run black, fix requirements.txt

--- integration_tests/v1/conftest.py
+++ integration_tests/v1/conftest.py
@@ -8,33 +8,33 @@ from boto3.session import Session
 from jsonschema import validate, exceptions
 from requests_aws4auth import AWS4Auth
 
-# TODO needs to come from an env var
-data_lpa_url_root = "https://uml2734a.dev.lpa.api.opg.service.justice.gov.uk/v1"
-healthcheck_url = f"{data_lpa_url_root}/healthcheck"
-online_tool_url = f"{data_lpa_url_root}/lpa-online-tool/lpas"
-use_an_lpa_url = f"{data_lpa_url_root}/use-an-lpa/lpas"
-request_code_url = f"{data_lpa_url_root}/use-an-lpa/lpas/requestCode"
+BRANCH_NAME = os.getenv("BRANCH_NAME")
+DATA_LPA_URL_ROOT = f"https://{BRANCH_NAME}.dev.lpa.api.opg.service.justice.gov.uk/v1"
+HEALTHCHECK_URL = f"{DATA_LPA_URL_ROOT}/healthcheck"
+ONLINE_TOOL_URL = f"{DATA_LPA_URL_ROOT}/lpa-online-tool/lpas"
+USE_AN_LPA_URL = f"{DATA_LPA_URL_ROOT}/use-an-lpa/lpas"
+REQUEST_CODE_URL = f"{DATA_LPA_URL_ROOT}/use-an-lpa/lpas/requestCode"
 
 opg_data_lpa_dev_aws = {
     "name": "new collections api on aws dev",
     "healthcheck_endpoint": {
-        "url": healthcheck_url,
+        "url": HEALTHCHECK_URL,
         "method": "GET",
     },
     "online_tool_endpoint": {
-        "url": online_tool_url,
+        "url": ONLINE_TOOL_URL,
         "method": "GET",
         "valid_lpa_online_tool_ids": ["A33718377316"],
         "invalid_lpa_online_tool_ids": ["banana"],
     },
     "use_an_lpa_endpoint": {
-        "url": use_an_lpa_url,
+        "url": USE_AN_LPA_URL,
         "method": "GET",
         "valid_sirius_uids": ["700000000047"],
         "invalid_sirius_uids": ["9"],
     },
     "request_code_endpoint": {
-        "url": request_code_url,
+        "url": REQUEST_CODE_URL,
         "method": "POST",
         "valid_sirius_lpas": [
             {"caseUid": 700000000013, "actorUid": 700000000997},
@@ -47,23 +47,23 @@ opg_data_lpa_dev_aws = {
 opg_data_lpa_local_mock = {
     "name": "new collections api local mock",
     "healthcheck_endpoint": {
-        "url": "http://0.0.0.0:4343/v1/healthcheck",
+        "url": "http://127.0.0.1:4343/v1/healthcheck",
         "method": "GET",
     },
     "online_tool_endpoint": {
-        "url": "http://0.0.0.0:4343/v1/lpa-online-tool/lpas",
+        "url": "http://127.0.0.1:4343/v1/lpa-online-tool/lpas",
         "method": "GET",
         "valid_lpa_online_tool_ids": ["A39721583862"],
         "invalid_lpa_online_tool_ids": ["banana"],
     },
     "use_an_lpa_endpoint": {
-        "url": "http://0.0.0.0:4343/v1/use-an-lpa/lpas",
+        "url": "http://127.0.01:4343/v1/use-an-lpa/lpas",
         "method": "GET",
         "valid_sirius_uids": ["700000000138"],
         "invalid_sirius_uids": ["9"],
     },
     "request_code_endpoint": {
-        "url": "http://0.0.0.0:4343/v1/use-an-lpa/lpas/requestCode",
+        "url": "http://127.0.0.1:4343/v1/use-an-lpa/lpas/requestCode",
         "method": "POST",
         "valid_sirius_lpas": [
             {"caseUid": 700000000138, "actorUid": 700000000997},
