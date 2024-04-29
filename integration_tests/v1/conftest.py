708ff951f78cf385eafdf1f4e3114f41d498d430
Nick Davis <nicholas.davis@digital.justice.gov.uk>
2024-04-29 14:49:39 +0100

    take out no longer relevant code

--- integration_tests/v1/conftest.py
+++ integration_tests/v1/conftest.py
@@ -8,31 +8,6 @@ from boto3.session import Session
 from jsonschema import validate, exceptions
 from requests_aws4auth import AWS4Auth
 
-opg_sirius_api_gateway_dev_aws = {
-    "name": "original collections api on aws dev",
-    "online_tool_endpoint": {
-        "url": "https://dev.lpa.api.opg.service.justice.gov.uk/v1/lpa-online-tool/lpas",
-        "method": "GET",
-        "valid_lpa_online_tool_ids": ["A33718377316"],
-        "invalid_lpa_online_tool_ids": ["banana"],
-    },
-    "use_an_lpa_endpoint": {
-        "url": "https://dev.lpa.api.opg.service.justice.gov.uk/v1/use-an-lpa/lpas",
-        "method": "GET",
-        "valid_sirius_uids": ["700000000013"],
-        "invalid_sirius_uids": ["9"],
-    },
-    "request_code_endpoint": {
-        "url": "https://dev.lpa.api.opg.service.justice.gov.uk/v1/lpas/requestCode",
-        "method": "POST",
-        "valid_sirius_lpas": [
-            {"caseUid": 700000000013, "actorUid": 700000000997},
-            {"caseUid": 700000000013, "actorUid": 700000000971},
-        ],
-    },
-}
-
-
 opg_data_lpa_dev_aws = {
     "name": "new collections api on aws dev",
     "healthcheck_endpoint": {
