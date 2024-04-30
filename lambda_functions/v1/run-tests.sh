462ba1af3b0371eec10442a5a78378f4ba52c342
Sam Ainsworth <samuel.ainsworth@digital.justice.gov.uk>
2024-04-30 16:35:58 +0100

    Fix unit tests

--- lambda_functions/v1/run-tests.sh
+++ lambda_functions/v1/run-tests.sh
@@ -2,6 +2,6 @@
 
 set -oe pipefail
 
-coverage run --source /lpa_data/lambda_functions/v1/functions/lpa/app/api -m pytest /lpa_data/lambda_functions/v1/tests/
+coverage run --source ${LAMBDA_TASK_ROOT}/lambda_functions/v1/functions/lpa/app/api -m pytest ${LAMBDA_TASK_ROOT}/lambda_functions/v1/tests
 
 coverage report
