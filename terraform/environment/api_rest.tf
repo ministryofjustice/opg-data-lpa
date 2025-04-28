d1533ade58c52e2c7971d9e4fc6cc05342f894b7
Andy Price <sixdaysandy@gmail.com>
2025-04-28 08:47:44 +0100

    SP-2781 - Externalise API Gateway Policy from OpenAPI Spec #minor - Externalise the APIGW Policy from the OpenAPI Spec - Trigger deployments on API Policy change - Add Conditional additional policy to IP Allow List Access - Pull in MOJ Allow List to allow specific account for preproduction - Now the Policy is handled by terraform directly refactor tfvars

--- terraform/environment/api_rest.tf
+++ terraform/environment/api_rest.tf
@@ -2,6 +2,7 @@ resource "aws_api_gateway_rest_api" "lpa" {
   name        = "lpa-${local.environment}"
   description = "API Gateway for LPA - ${local.environment}"
   body        = local.template_file
+  policy      = data.aws_iam_policy_document.lpa_rest_api_policy.json
 
   endpoint_configuration {
     types = ["REGIONAL"]
@@ -14,11 +15,65 @@ resource "aws_api_gateway_rest_api" "lpa" {
 
 resource "null_resource" "open_api" {
   triggers = {
-    open_api_sha = local.open_api_sha
+    open_api_sha        = local.open_api_sha
+    rest_api_policy_sha = local.rest_api_policy_sha
   }
 }
 
 locals {
-  template_file = templatefile(local.openapi_spec, local.api_template_vars)
-  open_api_sha  = substr(replace(base64sha256(local.template_file), "/[^0-9A-Za-z_]/", ""), 0, 5)
+  template_file       = templatefile(local.openapi_spec, local.api_template_vars)
+  open_api_sha        = substr(replace(base64sha256(local.template_file), "/[^0-9A-Za-z_]/", ""), 0, 5)
+  rest_api_policy_sha = substr(base64sha256(data.aws_iam_policy_document.lpa_rest_api_policy.json), 0, 5)
+}
+
+data "aws_iam_policy_document" "lpa_rest_api_policy" {
+  override_policy_documents = local.ip_restrictions_enabled ? [data.aws_iam_policy_document.lpa_rest_api_ip_restriction_policy[0].json] : []
+  statement {
+    sid    = "AllowExecuteByAllowedRoles"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = local.account.allowed_roles
+    }
+    actions   = ["execute-api:Invoke"]
+    resources = ["arn:aws:execute-api:eu-west-1:${local.account.account_id}:*/*/*/*"]
+  }
+}
+data "aws_iam_policy_document" "lpa_rest_api_ip_restriction_policy" {
+  count = local.ip_restrictions_enabled ? 1 : 0
+  statement {
+    sid    = "DenyExecuteByNoneAllowedIPRanges"
+    effect = "Deny"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions   = ["execute-api:Invoke"]
+    resources = ["arn:aws:execute-api:eu-west-1:${local.account.account_id}:*/*/*/*"]
+    condition {
+      test     = "NotIpAddress"
+      variable = "aws:SourceIp"
+      values   = local.allow_list_mapping[local.account.account_mapping]
+    }
+  }
+}
+
+module "allow_list" {
+  source = "git@github.com:ministryofjustice/opg-terraform-aws-moj-ip-allow-list.git?ref=v3.3.0"
+}
+
+locals {
+  allow_list_mapping = {
+    preproduction = concat(
+      module.allow_list.make_an_lpa_preproduction,
+      module.allow_list.use_an_lpa_preproduction,
+      module.allow_list.sirius_pre_allow_list,
+    )
+    production = concat(
+      module.allow_list.make_an_lpa_production,
+      module.allow_list.use_an_lpa_production,
+      module.allow_list.sirius_prod_allow_list,
+    )
+  }
+  ip_restrictions_enabled = contains(["preproduction"], local.account.account_mapping)
 }
