5cb3be049379d547b6e9432db56592fd20830912
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2025-09-11 13:11:29 +0100

    Replace motoserver with localstack
    Localstack is more commonly used and more familiar in OPG and can be configured more naturally.
    
    I also did some related cleanup:
    - Removing the unused postgres service
    - Fixing dependencies in docker-compose
    - Simplifying `up` targets in the Makefile
    - Removing the `create_secrets` target as this is done in localstack's init
    
    I needed to make one code change, which was to set the AWS region when using localstack-client: it was previously defaulting to us-east-1 which motoserver ignored but localstack is region-sensitive.
    
    Fixes CTC-234 #patch

--- lambda_functions/v1/functions/lpa/app/config.py
+++ lambda_functions/v1/functions/lpa/app/config.py
@@ -32,7 +32,6 @@ class LocalMockConfig(Config):
     REQUEST_CACHE_NAME = "opg-data-lpa-local"
 
     # specific to local testing
-    LOCALSTACK_HOST = "motoserver"
     JWT_SECRET = "THIS_IS_MY_SECRET_KEY"  # pragma: allowlist secret
 
 
@@ -51,5 +50,4 @@ class LocalTestingConfig(Config):
     REDIS_URL = "redis://redis:6379"
 
     # specific to local testing
-    LOCALSTACK_HOST = "motoserver"
     JWT_SECRET = "THIS_IS_MY_SECRET_KEY"  # pragma: allowlist secret
